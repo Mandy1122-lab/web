b856fbd384c206c07e15770bb1f709c3a112f62a
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-12-17 11:45:13 +0800

    Add files via upload

---
+++ drivelin/AuthContext.tsx
@@ -0,0 +1,55 @@
+'use client'
+import { createContext, useContext, useState } from "react";
+type authContextType = {
+  account:{
+    email: string;
+    roles: string[];
+  }
+  login: (email:string, roles: string[]) => void;
+  logout: () => void;
+};
+const authContextDefaultValues: authContextType = {
+  account:{
+    email: "",
+    roles: [],
+  },
+  login: (email:string) => {console.log(email)},
+  logout: () => {},
+};
+
+
+
+
+const AuthContext = createContext<authContextType>(authContextDefaultValues);
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [account, setAccount] = useState({email:"",roles:[] as string[]});
+  
+//   console.log('email:',account.email);
+  const login = (email:string, roles:string[]) => {
+      setAccount({email,roles});
+  };
+
+  const logout = () => {
+    setAccount({email:"",roles:[]});
+  };
+  const value = {
+    account,
+    login,
+    logout,
+};
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
